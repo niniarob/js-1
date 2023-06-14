e75fcfb3303881f9b89bc238266355d0ab5cecf2
unknown <robakidzenino89@gmail.com>
2023-06-14 12:18:23 +0400

    js

---
+++ index.js
@@ -0,0 +1,50 @@
+let season = "january";
+
+switch (season) {
+    case "january":
+    case "february":
+    case "december":
+      console.log("winter");
+      break;
+    case "march":
+    case "april":
+    case "may":
+      console.log("spring");
+      break;
+    case "june":
+    case "july":
+    case "august":
+      console.log("summer");
+      break;
+    case "september":
+    case "october":
+    case "november":
+      console.log("autumn");
+      break;
+    default:
+      console.log(" month");
+      break;
+  }
+
+
+
+
+// let season = "autum";
+
+// switch(season){
+//     case "january" ||  "february"|| "desember":
+//         console.log("winter")
+//         break;
+//     case "march" || "april" || "may":
+//         console.log("gazafxuli")
+//         break;
+//     case "june" || "july" || "august":
+//         console.log("summer")
+//         break;
+//     case "september" || "octomber"|| "november":
+//         console.log("autum")
+//         break;
+// }
+  
+
+// bith is a right ..
